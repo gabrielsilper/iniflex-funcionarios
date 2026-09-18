a563a7f0c7b119429968e1fdeaa4d5e70090de2c
gabrielsilper <gabrielsilper.main@gmail.com>
2026-09-17 22:26:48 -0400

    refactor: ajusta FuncionarioRepositoryImpl para ter a lista padrão ou injetar - Possível agora passar listar pelo construtor - Lista substituível para fazer os testes;

---
+++ src/test/java/com/github/gabrielsilper/repository/FuncionarioRepositoryImplTest.java
@@ -0,0 +1,11 @@
+package com.github.gabrielsilper.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class FuncionarioRepositoryImplTest {
+
+    @Test
+    public void deveListarTodosFuncionarios() {
+        // Test implementation
+    }
+}
